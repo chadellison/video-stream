522313a4b6ee6972d53094a399517d8f652f176c
Chad Ellison <chad.ellison0123@gmail.com>
2021-01-08 01:48:56 -0700

    update server route

--- src/baseServerUrl.js
+++ src/baseServerUrl.js
@@ -1,1 +1,1 @@
-export const WEBSOCKET_HOST = process.env.NODE_ENV === 'development' ? 'ws://localhost:3001/cable' : 'wss://relay_server.herokuapp.com/cable';
+export const WEBSOCKET_HOST = process.env.NODE_ENV === 'development' ? 'ws://localhost:3001/cable' : 'wss://artificial-vision-server.herokuapp.com/cable';
